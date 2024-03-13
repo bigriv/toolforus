3c9c90116ad5df888176869ea1e56380aca69e59
yoshiwo13 <pandemiccer@gmail.com>
2024-03-14 01:05:34 +0900

    add: [stopwatch]ストップウォッチ追加

---
+++ types/common/css/shadow.ts
@@ -0,0 +1,37 @@
+import { TOUColor } from "./color";
+
+export class TOUShadow {
+  vertical: number;
+  horizontal: number;
+  blur?: number;
+  spread?: number;
+  color: TOUColor;
+
+  constructor(define: {
+    vertical: number;
+    horizontal: number;
+    blur?: number;
+    spread?: number;
+    color?: TOUColor;
+  }) {
+    this.vertical = define.vertical;
+    this.horizontal = define.horizontal;
+    this.blur = define.blur;
+    this.spread = define.spread;
+    this.color = define.color ?? new TOUColor(TOUColor.CODE_BLACK);
+  }
+
+  converte(): string {
+    let style = "";
+    style += this.vertical + "rem ";
+    style += this.horizontal + "rem ";
+    if (this.blur) {
+      style += this.blur + "rem ";
+    }
+    if (this.spread) {
+      style += this.spread + "rem ";
+    }
+    style += this.color.rgba();
+    return style;
+  }
+}

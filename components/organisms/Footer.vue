960687a16775c996579869fbfa445a441c6b5cb3
yoshiwo13 <pandemiccer@gmail.com>
2024-03-26 15:32:07 +0900

    add: 仮のフッター追加

---
+++ components/organisms/Footer.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+const links = [
+  {
+    label: "利用規約",
+    link: "",
+  },
+  {
+    label: "プライバシーポリシー",
+    link: "",
+  },
+  {
+    label: "Q&A",
+    link: "",
+  },
+];
+</script>
+
+<template>
+  <footer>
+    <div class="c-footer">
+      <div class="c-footer__links">
+        <NuxtLink v-for="link in links" class="c-footer__links__link">{{
+          link.label
+        }}</NuxtLink>
+      </div>
+    </div>
+  </footer>
+</template>
+<style scoped lang="scss">
+.c-footer {
+  width: 100%;
+  height: 120px;
+  padding: 2% 5%;
+  display: flex;
+  background: #eee;
+  justify-content: center;
+  &__links {
+    display: flex;
+    flex-wrap: nowrap;
+    gap: 0.4rem 0.8rem;
+    &__link {
+      font-size: 0.6rem;
+      font-weight: bold;
+      text-decoration: underline;
+      cursor: pointer;
+      &:hover {
+        opacity: 0.8;
+      }
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,11 +3,13 @@
   <main>
     <slot />
   </main>
+  <OrganismsFooter />
 </template>
 
 <style scoped lang="scss">
 main {
   width: 100%;
   height: 100%;
+  padding: 2% 5%;
 }
 </style>

5d993966152ba24fedaeef78dd4ca76bb021d181
yoshiwo13 <pandemiccer@gmail.com>
2024-03-28 20:34:46 +0900

    add: コンポーネント追加 - コピーアイコン追加 - 説明アイコン追加

---
+++ components/molecules/icons/CopyIcon.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import BasicBalloon from "@/components/atoms/BasicBalloon.vue";
+
+const props = defineProps({
+  content: {
+    type: String,
+    default: undefined,
+  },
+});
+const emits = defineEmits(["copy"]);
+const onCopy = () => {
+  if (props.content) {
+    navigator.clipboard.writeText(props.content);
+  }
+  emits("copy");
+};
+</script>
+
+<template>
+  <div class="c-copy" @click="onCopy">
+    <BasicBalloon content="コピーしました。" timming="click">
+      <img src="/commons/icons/copy.svg" alt="コピー" />
+    </BasicBalloon>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.c-copy {
+  position: relative;
+  cursor: pointer;
+  vertical-align: middle;
+  width: 100%;
+  height: 100%;
+  img {
+      width: 100%;
+      height: 100%;
+
+  }
+  &:hover {
+    background: #eee;
+  }
+}
+</style>
---
+++ components/molecules/icons/DescriptionIcon.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import BasicBalloon from "@/components/atoms/BasicBalloon.vue";
+
+const props = defineProps({
+  icon: {
+    type: String as PropType<"info" | "help">,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+});
+const icon = computed(() => {
+  switch (props.icon) {
+    case "info":
+      return "/commons/icons/info.svg";
+    case "help":
+      return "/commons/icons/help.svg";
+  }
+});
+</script>
+
+<template>
+  <div class="c-icon">
+    <BasicBalloon :content="props.description">
+      <img :src="icon" class="u-icon--gray" />
+    </BasicBalloon>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.c-icon {
+  img {
+    vertical-align: middle;
+  }
+}
+</style>
